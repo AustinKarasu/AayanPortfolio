b6787a6e963c3e3f5713d8031a5e196405b10545
AustinKarasu <zyronetworkofficial@gmail.com>
2026-03-09 22:10:41 +0530

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,13 @@
+export async function getProjects(){
+
+ const res = await fetch("/api/projects")
+ return res.json()
+
+}
+
+export async function getUpdates(){
+
+ const res = await fetch("/api/updates")
+ return res.json()
+
+}
