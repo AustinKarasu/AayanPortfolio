c65ff2da129672ac8794e77ce10c43256fa3488c
AustinKarasu <zyronetworkofficial@gmail.com>
2026-03-09 22:37:22 +0530

    Create analytics.js

---
+++ functions/analytics.js
@@ -0,0 +1,11 @@
+export async function onRequest({ request, env }){
+
+ const ua = request.headers.get("user-agent")
+
+ await env.DB.prepare(
+  "INSERT INTO analytics(event) VALUES('visit')"
+ ).run()
+
+ return new Response("ok")
+
+}
