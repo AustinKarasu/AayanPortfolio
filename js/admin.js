7512585b12f6c398f3bb58923f05f71cc2408987
AustinKarasu <zyronetworkofficial@gmail.com>
2026-03-09 22:04:33 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,23 @@
+async function addProject(){
 
+ const title = document.getElementById("title").value
+ const image = document.getElementById("image").value
+ const url = document.getElementById("url").value
+
+ const res = await fetch("/content.json")
+ const data = await res.json()
+
+ data.projects.push({
+   title,
+   image,
+   url
+ })
+
+ await fetch("/api/updateContent",{
+   method:"POST",
+   body:JSON.stringify(data)
+ })
+
+ alert("Project Added")
+
+}
