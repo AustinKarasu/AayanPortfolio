499a906930bf3fc72387ea30d68a4067848a6f68
AustinKarasu <zyronetworkofficial@gmail.com>
2026-03-10 18:09:55 +0530

    Update study.js

--- functions/study.js
+++ functions/study.js
@@ -2,5 +2,7 @@ export async function onRequestGet({ env }) {
   const { results } = await env.DB.prepare(
     "SELECT * FROM content WHERE type = 'study' AND is_active = TRUE ORDER BY order_num ASC"
   ).all();
-  return Response.json(results);
+  return new Response(JSON.stringify(results), {
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+  });
 }
