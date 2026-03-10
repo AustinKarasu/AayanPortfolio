d4bf4ffaa51e6a8b507c47dd3c85123625131571
AustinKarasu <zyronetworkofficial@gmail.com>
2026-03-10 18:01:11 +0530

    Update projects.js

--- functions/projects.js
+++ functions/projects.js
@@ -1,9 +1,6 @@
-export async function onRequest({ env }) {
-
- const { results } = await env.DB.prepare(
-  "SELECT * FROM projects ORDER BY id DESC"
- ).all()
-
- return Response.json(results)
-
+export async function onRequestGet({ env }) {
+  const { results } = await env.DB.prepare(
+    "SELECT * FROM content WHERE type = 'project' AND is_active = TRUE ORDER BY order_num ASC"
+  ).all();
+  return Response.json(results);
 }
