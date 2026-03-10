d4f96be0609c59fde1a40a23bfdab424e3d59c47
AustinKarasu <zyronetworkofficial@gmail.com>
2026-03-10 18:01:25 +0530

    Update study.js

--- functions/study.js
+++ functions/study.js
@@ -1,9 +1,6 @@
-export async function onRequest({ env }) {
-
- const { results } = await env.DB.prepare(
-  "SELECT * FROM study_notes"
- ).all()
-
- return Response.json(results)
-
+export async function onRequestGet({ env }) {
+  const { results } = await env.DB.prepare(
+    "SELECT * FROM content WHERE type = 'study' AND is_active = TRUE ORDER BY order_num ASC"
+  ).all();
+  return Response.json(results);
 }
