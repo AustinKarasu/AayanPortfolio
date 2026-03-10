e1ea7d2a160d076d39d8d9766520c63f123bc0cd
AustinKarasu <zyronetworkofficial@gmail.com>
2026-03-10 18:32:34 +0530

    Delete functions directory

--- functions/study.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet({ env }) {
-  const { results } = await env.DB.prepare(
-    "SELECT * FROM content WHERE type = 'study' AND is_active = TRUE ORDER BY order_num ASC"
-  ).all();
-  return new Response(JSON.stringify(results), {
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
-  });
-}
--- functions/updates.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet({ env }) {
-  const { results } = await env.DB.prepare(
-    "SELECT * FROM announcements WHERE is_active = TRUE ORDER BY created_at DESC LIMIT 10"
-  ).all();
-  return new Response(JSON.stringify(results), {
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
-  });
-}
