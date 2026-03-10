ab80abe4ba6010395049d44fcd25090e1bd49f68
AustinKarasu <zyronetworkofficial@gmail.com>
2026-03-10 18:01:46 +0530

    Update updates.js

--- functions/updates.js
+++ functions/updates.js
@@ -1,9 +1,6 @@
-export async function onRequest({ env }) {
-
- const { results } = await env.DB.prepare(
-  "SELECT * FROM updates ORDER BY id DESC"
- ).all()
-
- return Response.json(results)
-
+export async function onRequestGet({ env }) {
+  const { results } = await env.DB.prepare(
+    "SELECT * FROM announcements WHERE is_active = TRUE ORDER BY created_at DESC LIMIT 10"
+  ).all();
+  return Response.json(results);
 }
